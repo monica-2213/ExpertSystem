3d193553a0fddb750da58bf75354af097ca64991
EVANGELINE MONICA A/P RAJEN <93172320+monica-2213@users.noreply.github.com>
2023-05-25 13:29:49 +0800

    Update cervicalCancer.py

--- cervicalCancer.py
+++ cervicalCancer.py
@@ -120,7 +120,6 @@ knowledge_base = {
     }
 }
 
-
 def calculate_cervical_cancer_risk(answers):
     # Calculate the risk score based on the answers
     risk_score = 0
@@ -133,9 +132,8 @@ def calculate_cervical_cancer_risk(answers):
                 risk_score += rule_score
 
     # Calculate risk percentage
-    risk_percentage = (risk_score / 200) * 100
+    risk_percentage = (risk_score / sum([factor['risk_factor'] for factor in knowledge_base.values()])) * 100
     return risk_percentage
-
 
 def main():
     st.title('Cervical Cancer Risk Assessment')
@@ -144,50 +142,36 @@ def main():
     questions = {
         'age': 'How old are you?',
         'multiple_partners': 'Have you ever had multiple sexual partners?',
-        'early_sexual_activity': 'Did you engage in sexual activity at an early age?',
-        'hpv_infection': 'Have you ever been diagnosed with HPV infection?',
-        'safe_sex': 'Do you consistently practice safe sex and use condoms?',
-        'smoking': 'Do you currently smoke?',
-        'weakened_immune_system': 'Do you have a weakened immune system due to a medical condition or immunosuppressant medications?',
-        'long_term_oral_contraceptives': 'Have you used oral contraceptives for a long time?',
-        'diet': 'How would you describe your diet?',
-        'obesity_overweight': 'Do you have obesity or are you overweight?',
-        'physical_activity': 'How would you describe your physical activity level?',
-        'family_history': 'Are there any close relatives (mother, sister, etc.) who have been diagnosed with cervical cancer?',
-        'gene_variations': 'Have you been tested for specific gene variations associated with cervical cancer susceptibility?',
-        'lynch_or_cowden_syndrome': 'Have you been diagnosed with Lynch syndrome or Cowden syndrome?',
-        'abnormal_bleeding': 'Have you experienced abnormal vaginal bleeding?',
-        'unusual_discharge': 'Have you noticed unusual vaginal discharge that is watery, bloody, or has a foul odor?',
-        'pelvic_pain': 'Have you been experiencing persistent pelvic pain in the pelvis, lower back, or abdomen?',
-        'pain_during_intercourse': 'Do you experience pain during sexual intercourse (dyspareunia)?',
-        'urinary_problems': 'Have you experienced urinary problems such as blood in the urine (hematuria), urinary incontinence, or frequent urination?'
+        # Add more questions here
     }
 
     answers = {}
 
     for key, question in questions.items():
-        if key == 'age':
-            answers[key] = st.number_input(question, min_value=18, max_value=100, step=1)
-        elif key == 'diet':
-            answers[key] = st.selectbox(question, [
-                'Low in fruits and vegetables, high in processed foods',
-                'Balanced and healthy'
-            ])
-        elif key == 'physical_activity':
-            answers[key] = st.selectbox(question, [
-                'Sedentary',
-                'Moderately active',
-                'Regularly active and engage in physical exercise'
-            ])
-        else:
-            answers[key] = st.radio(question, ['Yes', 'No'])
+        answers[key] = st.radio(question, ['Yes', 'No'])
 
     if st.button('Submit'):
         risk_percentage = calculate_cervical_cancer_risk(answers)
 
         st.write('Risk Assessment:')
-        st.write(f'Based on your responses, your risk of developing cervical cancer is {risk_percentage:.2f}%.')
+        st.write(f'Your risk score for cervical cancer: {risk_percentage:.2f}%')
 
+        if risk_percentage >= 50:
+            st.warning('Based on your risk score, you have a relatively higher risk for cervical cancer. Please consult with your healthcare provider for further evaluation and recommendations.')
+        else:
+            st.success('Based on your risk score, you have a relatively lower risk for cervical cancer. However, it is still important to attend regular screenings and maintain a healthy lifestyle.')
+
+        # Provide uncertainty estimation
+        st.write(f'Uncertainty: High')  # Add appropriate uncertainty measure or explanation
+
+        # Generate explanation
+        st.write('Explanation:')
+        st.write('Your risk score is calculated based on various risk factors for cervical cancer. The higher the risk score, the higher the probability of developing cervical cancer. The factors that contributed most to your risk score include...')
+        # Provide explanation for the factors contributing to the risk score
+
+        # Rule editor (dummy implementation)
+        if st.button('Edit Rules'):
+            st.write('Rule Editor: (Under development)')  # Provide an interface to edit the rules or add custom rules
 
 if __name__ == '__main__':
     main()
