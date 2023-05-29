ef081c47bb5d1e408d75aaa8d5c4e45c3ae6b226
EVANGELINE MONICA A/P RAJEN <93172320+monica-2213@users.noreply.github.com>
2023-05-29 11:16:56 +0800

    Update cervicalCancer.py

--- cervicalCancer.py
+++ cervicalCancer.py
@@ -250,10 +250,29 @@ def generate_explanation(factor_scores):
         explanation += f"- {factor}: {score}\n"
     return explanation
 
+def recommend_medical_tests():
+    st.header('Recommended Medical Tests and Screenings')
+    st.markdown('- Pap test: Recommended for all individuals with a cervix, starting at the age of 21 or within 3 years of becoming sexually active. It should be repeated every 3 years for individuals aged 21-65 who have a normal result.')
+    st.markdown('- HPV testing: In addition to the Pap test, HPV testing may be recommended for individuals aged 30 and above as part of cervical cancer screening. Talk to your healthcare provider for more information about HPV testing and its frequency.')
 
+def provide_treatment_recommendations():
+    st.header('Treatment Recommendations')
+    st.markdown('- If your risk score indicates a higher risk for cervical cancer, it is important to consult with your healthcare provider for further evaluation and recommendations.')
+    st.markdown('- Treatment options for cervical cancer may include surgery, radiation therapy, chemotherapy, or a combination of these approaches. The choice of treatment depends on the stage of cancer, overall health, and individual preferences. Your healthcare provider will guide you through the treatment decision-making process.')
+
+def provide_helplines():
+    st.header('Helplines (Malaysia)')
+    st.markdown('- Cancer Helpline: 1-800-88-1000')
+    st.markdown('- National Population and Family Development Board (LPPKN): 03-7953 6655')
+    st.markdown('- Malaysian AIDS Council: 03-4047 7000')
+    st.markdown('- Talian Kasih: 15999 (24-hour helpline for survivors of domestic violence, sexual abuse, and other related issues)')
+
+    
 def main():
     layout()
-    
+    recommend_medical_tests()
+    provide_treatment_recommendations()
+    provide_helplines()
     
 if __name__ == '__main__':
     main()
