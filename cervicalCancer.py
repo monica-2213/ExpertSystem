8b8b4365c6c3dd3558d34fc31b48c9433092b298
EVANGELINE MONICA A/P RAJEN <93172320+monica-2213@users.noreply.github.com>
2023-05-31 22:03:44 +0800

    Update cervicalCancer.py

--- cervicalCancer.py
+++ cervicalCancer.py
@@ -7,7 +7,7 @@ st.markdown(
     """
     <style>
     .stApp {
-        background-image: linear-gradient(rgba(0,0,0,0.2),#fff5f8),url(https://images.template.net/121244/international-women-s-day-design-background-nuejr.jpg);
+        background-image: linear-gradient(rgba(0,0,0,0.5),#fff5f8),url(https://images.template.net/121244/international-women-s-day-design-background-nuejr.jpg);
         background-position: center;
         background-size: cover;
     }
