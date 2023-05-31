783ee864585e74adb18cedfe238691c00d37a001
EVANGELINE MONICA A/P RAJEN <93172320+monica-2213@users.noreply.github.com>
2023-05-31 21:45:49 +0800

    Update cervicalCancer.py

--- cervicalCancer.py
+++ cervicalCancer.py
@@ -279,7 +279,7 @@ def layout():
         st.markdown('#### Urinary Problems')
         urinary_problems = st.radio('Have you experienced urinary problems such as blood in the urine (hematuria), urinary incontinence, or frequent urination?', ['Yes', 'No'])
     
-    if st.button("Submit", class="colorful-button"):
+    if st.button('Submit'):
         answers = {
             'age': age,
             'multiple_partners': multiple_partners,
