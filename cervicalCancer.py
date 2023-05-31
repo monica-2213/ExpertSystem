8ef9322d5636b6d4ae88a060481377cf45704ecc
EVANGELINE MONICA A/P RAJEN <93172320+monica-2213@users.noreply.github.com>
2023-05-31 20:57:53 +0800

    Update cervicalCancer.py

--- cervicalCancer.py
+++ cervicalCancer.py
@@ -328,7 +328,7 @@ def recommend_medical_tests():
         """,
         unsafe_allow_html=True
     )
-    st.markdown('<p class="header-text">Recommended Medical Tests and Screenings', unsafe_allow_html=True)
+    st.write('Recommended Medical Tests and Screenings')
     st.markdown('<p class="content-text">Pap test: Recommended for all individuals with a cervix, starting at the age of 21 or within 3 years of becoming sexually active. It should be repeated every 3 years for individuals aged 21-65 who have a normal result.</p>', unsafe_allow_html=True)
     st.markdown('<p class="content-text">- HPV testing: In addition to the Pap test, HPV testing may be recommended for individuals aged 30 and above as part of cervical cancer screening. Talk to your healthcare provider for more information about HPV testing and its frequency.</p>', unsafe_allow_html=True)
 
@@ -349,7 +349,7 @@ def provide_treatment_recommendations():
     )
     
     # Use Markdown strings directly without HTML tags
-    st.markdown('<p class="header-text">Treatment Recommendations</p>', unsafe_allow_html=True)
+    st.write('Treatment Recommendations')
     st.markdown('<p class="content-text">- If your risk score indicates a higher risk for cervical cancer, it is important to consult with your healthcare provider for further evaluation and recommendations.</p>', unsafe_allow_html=True)
     st.markdown('<p class="content-text">- Treatment options for cervical cancer may include surgery, radiation therapy, chemotherapy, or a combination of these approaches. The choice of treatment depends on the stage of cancer, overall health, and individual preferences. Your healthcare provider will guide you through the treatment decision-making process.</p>', unsafe_allow_html=True)
 
@@ -368,7 +368,7 @@ def provide_helplines():
         """,
         unsafe_allow_html=True
     )
-    st.markdown('<p class="header-text">Helplines (Malaysia)', unsafe_allow_html=True)
+    st.header('Helplines (Malaysia)')
     st.markdown('<p class="content-text">- Cancer Helpline: 1-800-88-1000</p>', unsafe_allow_html=True)
     st.markdown('<p class="content-text">- National Population and Family Development Board (LPPKN): 03-7953 6655</p>', unsafe_allow_html=True)
     st.markdown('<p class="content-text">- Malaysian AIDS Council: 03-4047 7000</p>', unsafe_allow_html=True)
